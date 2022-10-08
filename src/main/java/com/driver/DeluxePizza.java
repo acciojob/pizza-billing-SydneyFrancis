3c90504d41a16c2c35623d56a7f5880dcb09c5a6
SYDNEY FRANCIS <110589231+SydneyFrancis@users.noreply.github.com>
2022-10-08 10:42:36 +0530

    First Try

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -1,9 +1,28 @@
 package com.driver;
 
 public class DeluxePizza extends Pizza {
+    Boolean flag = true;
 
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
         // your code goes here
+    }
+
+    @Override
+    public void addExtraCheese() {
+//        super.addExtraCheese();
+    }
+
+    @Override
+    public void addExtraToppings() {
+//        super.addExtraToppings();
+    }
+
+    public void addTakeaway(){
+        // your code goes here
+        if(flag){
+            flag = false;
+            super.addTakeaway();
+        }
     }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -8,7 +8,21 @@ public class Pizza {
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
+        if(isVeg){
+            setPrice(300);
+        }
+        else{
+            setPrice(400);
+        }
         // your code goes here
+    }
+
+    public void setPrice(int price) {
+        if(this.isVeg){
+            this.price = this.price + price;
+        }else{
+            this.price = this.price + price;
+        }
     }
 
     public int getPrice(){
@@ -17,18 +31,32 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        int price = 80;
+        setPrice(price);
+        price = 0;
     }
 
     public void addExtraToppings(){
         // your code goes here
+        int price = 70;
+        setPrice(price);
+        price = 0;
     }
 
     public void addTakeaway(){
         // your code goes here
+        int price = 20;
+        setPrice(price);
+        price = 0;
     }
 
     public String getBill(){
         // your code goes here
+        setBill(String.valueOf(this.price)+"\n");
         return this.bill;
+    }
+
+    public void setBill(String bill) {
+        this.bill = bill;
     }
 }
